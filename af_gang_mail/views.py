b42f38ecc4b3d0048df00996f1eebd02e480a555
Craig Anderson <craiga@craiga.id.au>
2020-06-22 18:05:34 +0100

    WIP

--- af_gang_mail/views.py
+++ af_gang_mail/views.py
@@ -2,6 +2,12 @@
 
 from django.views.generic import TemplateView
 
+from allauth.account.forms import LoginForm, SignupForm
+
 
 class Home(TemplateView):
     template_name = "af_gang_mail/home.html"
+    extra_context = {
+        "login_form": LoginForm(),
+        "register_form": SignupForm(),
+    }
