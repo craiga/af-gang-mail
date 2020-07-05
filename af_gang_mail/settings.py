1ec9972574218e086ebc78f42e1668148ddc94e2
Craig Anderson <craiga@craiga.id.au>
2020-07-05 19:41:04 +0100

    Natural day. Fixes #81.

--- af_gang_mail/settings.py
+++ af_gang_mail/settings.py
@@ -53,10 +53,11 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",
+    "django.contrib.humanize",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.sessions",
     "django.contrib.sites",
+    "django.contrib.staticfiles",
     # Third-party
     "allauth",
     "allauth.account",
