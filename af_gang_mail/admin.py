bf2ae056a6d3d86ed296cc65bd2ce993366f0bae
Craig Anderson <craiga@craiga.id.au>
2020-09-16 11:16:49 +0100

    Add address to admin.

--- af_gang_mail/admin.py
+++ af_gang_mail/admin.py
@@ -10,6 +10,21 @@ class UserAdmin(auth.admin.UserAdmin):
     change_form_template = "admin/auth/user/change_form.html"
     change_list_template = "admin/auth/user/change_list.html"
     list_filter = ["address_country"]
+    fieldsets = auth.admin.UserAdmin.fieldsets + (
+        (
+            "Address",
+            {
+                "fields": [
+                    "address_line_1",
+                    "address_line_2",
+                    "address_city",
+                    "address_state",
+                    "address_postcode",
+                    "address_country",
+                ]
+            },
+        ),
+    )
 
 
 @admin.register(models.Draw)
