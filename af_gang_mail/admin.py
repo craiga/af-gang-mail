6469805718f9235aa53245ea97d5f8b6d31df52d
Craig Anderson <craiga@craiga.id.au>
2020-09-16 10:41:46 +0100

    Filter users by country

--- af_gang_mail/admin.py
+++ af_gang_mail/admin.py
@@ -9,6 +9,7 @@ from af_gang_mail import models
 class UserAdmin(auth.admin.UserAdmin):
     change_form_template = "admin/auth/user/change_form.html"
     change_list_template = "admin/auth/user/change_list.html"
+    list_filter = ["address_country"]
 
 
 @admin.register(models.Draw)
